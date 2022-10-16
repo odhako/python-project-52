5672ab3c22bc0f131e654bc5e763e17c8b86612f
odhako <odhako@gmail.com>
2022-10-16 13:54:06 +0400

    Step 3: add user update

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -20,4 +20,5 @@ from task_manager.users import views
 urlpatterns = [
     path('', views.UsersView.as_view()),
     path('create/', views.CreateUserView.as_view()),
+    path('<int:pk>/update/', views.UpdateUserView.as_view())
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView, ListView
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import FormView, CreateView, UpdateView
 from django.contrib.auth.views import LoginView as BasicLoginView
 from django.shortcuts import redirect
 from .forms import UserRegistrationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext as _
+from django.utils.translation import pgettext
 
 
 # Create your views here.
@@ -24,3 +25,20 @@ class CreateUserView(SuccessMessageMixin, CreateView):
     form_class = UserRegistrationForm
     success_url = '/'
     success_message = _("User successfully registered")
+    extra_context = {
+        'header': _('Registration'),
+        'button': pgettext('sign up button', 'Sign up'),
+    }
+
+
+class UpdateUserView(SuccessMessageMixin, UpdateView):
+    model = User
+    template_name = 'create_user.html'
+    form_class = UserRegistrationForm
+    success_url = '/'
+    success_message = _("User successfully updated")
+    # rus: Пользователь успешно изменён
+    extra_context = {
+        'header': _('Update user'),
+        'button': _('Update'),
+    }
