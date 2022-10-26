12185b985f1c6ade1241982305b985605748c249
odhako <odhako@gmail.com>
2022-10-26 14:07:39 +0400

    Add Status model

--- task_manager/task_manager/models.py
+++ task_manager/task_manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import pgettext as _
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Status(TimeStampedModel):
+    name = models.CharField(verbose_name=_('Status model field', 'Name'),
+                            max_length=100,
+                            unique=True,
+                            null=False)
