43aa7f134fd9dc4b6f12bb7e11ff547e97a535ed
odhako <odhako@gmail.com>
2022-10-27 12:10:59 +0400

    Translation 2

--- task_manager/core/models.py
+++ task_manager/core/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.utils.translation import pgettext as _
+from django.utils.translation import gettext as _
 from django_extensions.db.models import TimeStampedModel
 
 
 class Status(TimeStampedModel):
-    name = models.CharField(verbose_name=_('Status model field', 'Name'),
-                            max_length=100,
-                            unique=True,
-                            null=False)
+    name = models.CharField(
+        verbose_name=_('Name'),
+        # verbose_name='Имя',
+        max_length=100,
+        unique=True,
+        null=False
+    )
 
     def __str__(self):
         return self.name
--- task_manager/core/views.py
+++ task_manager/core/views.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 from .models import Status
 from .forms import StatusForm
 from django.contrib.messages.views import SuccessMessageMixin
-from django.utils.translation import pgettext as _
+from django.utils.translation import pgettext
+from django.utils.translation import gettext as _
 
 
 class StatusesList(ListView):
@@ -16,12 +17,12 @@ class CreateStatus(SuccessMessageMixin, CreateView):
     template_name = 'create_user.html'
     form_class = StatusForm
     success_url = '/statuses/'
-    success_message = _('Success message', "Status successfully created")
+    success_message = _("Status successfully created")
     # rus: Статус успешно создан
     extra_context = {
-        'header': _('Create status page header', 'Create status'),
+        'header': pgettext('Create status page header', 'Create status'),
         # rus: Создать статус
-        'button': _('Create status button', 'Create'),
+        'button': pgettext('Create status button', 'Create'),
         # rus: Создать
     }
 
@@ -31,12 +32,12 @@ class UpdateStatus(SuccessMessageMixin, UpdateView):
     form_class = StatusForm
     model = Status
     success_url = '/statuses/'
-    success_message = _('Success message', "Status successfully updated")
+    success_message = _("Status successfully updated")
     # rus: Статус успешно изменён
     extra_context = {
-        'header': _('Update status page header', 'Update status'),
+        'header': pgettext('Update status page header', 'Update status'),
         # rus: Изменение статуса
-        'button': _('Update status button', 'Update'),
+        'button': pgettext('Update status button', 'Update'),
         # rus: Изменить
     }
 
@@ -45,11 +46,11 @@ class DeleteStatus(SuccessMessageMixin, DeleteView):
     template_name = 'delete.html'
     model = Status
     success_url = '/statuses/'
-    success_message = _('Success message', 'Status successfully deleted')
+    success_message = _('Status successfully deleted')
     # rus: Статус успешно удалён
     extra_context = {
-        'header': _('Delete status page header', 'Delete status'),
+        'header': pgettext('Delete status page header', 'Delete status'),
         # rus: Удаление статуса
-        'button': _('Delete status button', 'Yes, delete'),
+        'button': pgettext('Delete status button', 'Yes, delete'),
         # rus: Да, удалить
     }
