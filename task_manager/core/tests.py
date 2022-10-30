f746b3898239e0f0ba96e293e3eeb9cc172f6689
odhako <odhako@gmail.com>
2022-10-30 13:27:35 +0400

    Add test for statuses

--- task_manager/core/tests.py
+++ task_manager/core/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Status
 
 # Create your tests here.
+
+
+class StatusesTest(TestCase):
+
+    def setUp(self):
+        self.client.post(
+            '/users/create/',
+            data={
+                'first_name': 'Batman',
+                'last_name': 'Suparman',
+                'username': 'user_666',
+                'password1': 'password_666',
+                'password2': 'password_666',
+            }
+        )
+        self.client.login(username='user_666', password='password_666')
+
+    def test_crud_status(self):
+        self.client.post('/statuses/create/', data={'name': 'Burned in hell',})
+        status = Status.objects.get(name='Burned in hell')
+        self.assertEqual(status.name, 'Burned in hell')
+        print('Status create OK')
+
+        self.client.post(f'/statuses/{status.id}/update/', data={'name': 'Lol'})
+        status = Status.objects.get(id=status.id)
+        self.assertEqual(status.name, 'Lol')
+        print('Status update OK')
+
+        self.client.post(f'/statuses/{status.id}/delete/')
+        self.assertFalse(Status.objects.filter(id=status.id).exists())
+        print('Status delete OK')
+
+    def test_logged_out(self):
+        c = Client()
+        c.post('/logout/')
+        response = c.get('/statuses/', follow=True)
+        self.assertRedirects(response, '/login/?next=/statuses/')
+        print('Redirect unauthorised from /statuses/ OK')
