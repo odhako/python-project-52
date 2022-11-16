9f30c60ddb34ddb9f87cf05af36a720f77862a8d
odhako <odhako@gmail.com>
2022-11-16 12:45:43 +0400

    Translation

--- task_manager/core/forms.py
+++ task_manager/core/forms.py
@@ -2,6 +2,7 @@ from .models import Status, Task, Label
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django import forms
+from django.utils.translation import gettext as _, pgettext
 
 
 class StatusForm(ModelForm):
@@ -25,10 +26,23 @@ class LabelForm(ModelForm):
 
 
 class TaskFilter(forms.Form):
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False)
-    executor = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
-    label = forms.ModelChoiceField(queryset=Label.objects.all(), required=False)
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        required=False,
+        label=pgettext('Status filter', 'Status')
+    )
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label=pgettext('Executor filter', 'Executor')
+    )
+    label = forms.ModelChoiceField(
+        queryset=Label.objects.all(),
+        required=False,
+        label=pgettext('Label filter', 'Label')
+    )
     self_tasks = forms.BooleanField(
         widget=forms.CheckboxInput(),
         required=False,
+        label=pgettext('Self tasks filter', 'Self tasks')
     )
