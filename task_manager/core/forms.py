6b5350f29d53f1d0353bd31aa4ed2d151fc9fc0d
odhako <odhako@gmail.com>
2022-11-16 11:13:15 +0400

    Bootstrap form

--- task_manager/core/forms.py
+++ task_manager/core/forms.py
@@ -4,62 +4,31 @@ from django.forms import ModelForm
 from django import forms
 
 
-class NoLabelSuffixMixin:
-    def __init__(self, *args, **kwargs):
-        if 'label_suffix' not in kwargs:
-            kwargs['label_suffix'] = ''
-        super().__init__(*args, **kwargs)
-
-
-class PlaceholderMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        field_names = [field_name for field_name, _ in self.fields.items()]
-        for field_name in field_names:
-            field = self.fields.get(field_name)
-            field.widget.attrs.update({'placeholder': field.label})
-
-
-class FormControlMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            if field.widget.attrs.get('class'):
-                field.widget.attrs['class'] += ' form-control'
-            else:
-                field.widget.attrs['class'] = 'form-control'
-
-
-class StatusForm(NoLabelSuffixMixin,
-                 PlaceholderMixin,
-                 FormControlMixin,
-                 ModelForm):
+class StatusForm(ModelForm):
     class Meta:
         model = Status
         fields = ['name', ]
 
 
-class TaskForm(NoLabelSuffixMixin,
-               PlaceholderMixin,
-               FormControlMixin,
-               ModelForm):
+class TaskForm(ModelForm):
 
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
 
 
-class LabelForm(NoLabelSuffixMixin,
-                PlaceholderMixin,
-                FormControlMixin,
-                ModelForm):
+class LabelForm(ModelForm):
 
     class Meta:
         model = Label
         fields = ['name', ]
 
 
-class TaskFilter(NoLabelSuffixMixin, FormControlMixin, forms.Form):
+class TaskFilter(forms.Form):
     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False)
     executor = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
     label = forms.ModelChoiceField(queryset=Label.objects.all(), required=False)
+    self_tasks = forms.BooleanField(
+        widget=forms.CheckboxInput(),
+        required=False,
+    )
--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,35 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
-class NoLabelSuffixMixin:
-    def __init__(self, *args, **kwargs):
-        if 'label_suffix' not in kwargs:
-            kwargs['label_suffix'] = ''
-        super(NoLabelSuffixMixin, self).__init__(*args, **kwargs)
-
-
-class PlaceholderMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        field_names = [field_name for field_name, _ in self.fields.items()]
-        for field_name in field_names:
-            field = self.fields.get(field_name)
-            field.widget.attrs.update({'placeholder': field.label})
-
-
-class FormControlMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            if field.widget.attrs.get('class'):
-                field.widget.attrs['class'] += ' form-control'
-            else:
-                field.widget.attrs['class'] = 'form-control'
-
-
-class UserForm(
-    FormControlMixin, NoLabelSuffixMixin, PlaceholderMixin, UserCreationForm
-):
+class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = (
@@ -42,7 +14,5 @@ class UserForm(
         )
 
 
-class UserLoginForm(
-    FormControlMixin, NoLabelSuffixMixin, PlaceholderMixin, AuthenticationForm
-):
+class UserLoginForm(AuthenticationForm):
     pass
