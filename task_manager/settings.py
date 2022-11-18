fdaa0c6f9f3965ead8c977ec9b354e3ed63ae264
odhako <odhako@gmail.com>
2022-11-18 16:37:07 +0400

    Deploying on Railway.app - attempt 2 - tests not running

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -105,8 +105,8 @@ if "DATABASE_URL" in os.environ:
         conn_max_age=MAX_CONN_AGE, ssl_require=True)
 
     # Enable test database if found in CI environment.
-    if "CI" in os.environ:
-        DATABASES["default"]["TEST"] = DATABASES["default"]
+    # if "CI" in os.environ:
+    #     DATABASES["default"]["TEST"] = DATABASES["default"]
 
 
 # Password validation
