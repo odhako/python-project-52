d149debc7f7e69a4aa54ccdbd00e3ec46fb8f92d
odhako <odhako@gmail.com>
2022-11-23 08:18:41 +0400

    Remove unused import

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from task_manager import views
 from task_manager import views_tasks, views_users, views_labels, views_statuses
 
