f02b8aa247ddd249ea53dae240053da95ef4a576
odhako <odhako@gmail.com>
2022-11-30 09:28:05 +0400

    Fix style

--- task_manager/filters.py
+++ task_manager/filters.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import pgettext
 from django import forms
 
-from .models import Task, Status, Label
+from .models import Status, Label
 
 
 class UserModelChoiceField(forms.ModelChoiceField):
