31f39a03c9b41cac2d95cfdafb669bd59fa4a299
odhako <odhako@gmail.com>
2022-12-23 13:06:02 +0400

    Change import

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
+from task_manager.users.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
