4460eb0612d843791327d4318d13e2b6b478c297
odhako <odhako@gmail.com>
2023-01-22 15:22:08 +0400

    Add aliases to clean up urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,32 +16,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import task_manager.labels.views
-import task_manager.statuses.views
-import task_manager.users.views
+import task_manager.labels.views as views_labels
+import task_manager.statuses.views as views_statuses
+import task_manager.tasks.views as views_tasks
+import task_manager.users.views as views_users
 from task_manager import views
-from task_manager import views_tasks, views_labels
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view()),
-    path('users/', task_manager.users.views.UsersView.as_view()),
-    path('users/create/', task_manager.users.views.CreateUserView.as_view()),
-    path('users/<int:pk>/update/', task_manager.users.views.UpdateUserView.as_view()),
-    path('users/<int:pk>/delete/', task_manager.users.views.DeleteUserView.as_view()),
-    path('login/', task_manager.users.views.LoginUserView.as_view()),
-    path('logout/', task_manager.users.views.LogoutUserView.as_view()),
-    path('statuses/', task_manager.statuses.views.StatusesList.as_view()),
-    path('statuses/create/', task_manager.statuses.views.CreateStatus.as_view()),
-    path('statuses/<int:pk>/update/', task_manager.statuses.views.UpdateStatus.as_view()),
-    path('statuses/<int:pk>/delete/', task_manager.statuses.views.DeleteStatus.as_view()),
+    path('users/', views_users.UsersView.as_view()),
+    path('users/create/', views_users.CreateUserView.as_view()),
+    path('users/<int:pk>/update/', views_users.UpdateUserView.as_view()),
+    path('users/<int:pk>/delete/', views_users.DeleteUserView.as_view()),
+    path('login/', views_users.LoginUserView.as_view()),
+    path('logout/', views_users.LogoutUserView.as_view()),
+    path('statuses/', views_statuses.StatusesList.as_view()),
+    path('statuses/create/', views_statuses.CreateStatus.as_view()),
+    path('statuses/<int:pk>/update/', views_statuses.UpdateStatus.as_view()),
+    path('statuses/<int:pk>/delete/', views_statuses.DeleteStatus.as_view()),
     path('tasks/', views_tasks.TasksList.as_view()),
     path('tasks/create/', views_tasks.CreateTask.as_view()),
     path('tasks/<int:pk>/update/', views_tasks.UpdateTask.as_view()),
     path('tasks/<int:pk>/delete/', views_tasks.DeleteTask.as_view()),
     path('tasks/<int:pk>/', views_tasks.TaskView.as_view()),
-    path('labels/', task_manager.labels.views.LabelsList.as_view()),
-    path('labels/create/', task_manager.labels.views.CreateLabel.as_view()),
-    path('labels/<int:pk>/update/', task_manager.labels.views.UpdateLabel.as_view()),
-    path('labels/<int:pk>/delete/', task_manager.labels.views.DeleteLabel.as_view()),
+    path('labels/', views_labels.LabelsList.as_view()),
+    path('labels/create/', views_labels.CreateLabel.as_view()),
+    path('labels/<int:pk>/update/', views_labels.UpdateLabel.as_view()),
+    path('labels/<int:pk>/delete/', views_labels.DeleteLabel.as_view()),
 ]
