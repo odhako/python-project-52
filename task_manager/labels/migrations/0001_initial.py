09e46fa909d3880e34b68a58e3d761298bbca45b
odhako <odhako@gmail.com>
2023-01-22 15:38:08 +0400

    New migrations

--- task_manager/labels/migrations/0001_initial.py
+++ task_manager/labels/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Status',
+            name='Label',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
